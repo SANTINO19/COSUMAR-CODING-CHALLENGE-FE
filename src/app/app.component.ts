0bbddc12a599be5183e485740ebab6d4a39e57f4
SANTINO19 <59202534+SANTINO19@users.noreply.github.com>
2020-12-31 01:38:41 +0100

    Front end
    Front-end part of project with Angular Framewrok

---
+++ src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: 'app.component.html',
+  styleUrls: ['app.component.css'],
+})
+export class AppComponent {
+  title = 'Cosumar Coding Challenge';
+  th1 = 'ID:';
+  th2 = 'Name:';
+  th3 = 'Code:';
+  lat = 33.5731104;
+  lng = -7.5898434;
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { AgmCoreModule } from '@agm/core';
+
+import { AppComponent } from './app.component';
+
+@NgModule({
+  imports: [
+    BrowserModule,
+    AgmCoreModule.forRoot({
+      apiKey: ''
+    })
+  ],
+  providers: [],
+  declarations: [ AppComponent ],
+  bootstrap: [ AppComponent ]
+})
+export class AppModule {}
